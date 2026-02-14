43e31253ed3bfc80a732812654597a629ecd6ff2
Aditi Goyal <102579070+aditi5050@users.noreply.github.com>
2026-02-15 00:38:13 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -12,9 +12,9 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    const { userId } = await auth();
-
-    if (!userId) {
+    const authObj = await auth();
+    
+    if (!authObj?.userId) {
       const signInUrl = new URL("/signin", req.url);
       signInUrl.searchParams.set("redirect_url", req.url);
       return NextResponse.redirect(signInUrl);
@@ -23,6 +23,11 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
 
