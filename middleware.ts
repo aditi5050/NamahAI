5f5a864b77cc5e26143a13f9caf1eb9f359d92a4
Aditi Goyal <102579070+aditi5050@users.noreply.github.com>
2026-02-14 23:55:53 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/", 
-  "/signin(.*)", 
-  "/signup(.*)", 
+  "/",
+  "/signin(.*)",
+  "/signup(.*)",
   "/sso-callback(.*)",
   "/assets(.*)",
   "/api/upload(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  // Only protect non-public routes
+
   if (!isPublicRoute(req)) {
-    auth.protect();
+    auth().protect();   
   }
+
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)"],
 };
-
